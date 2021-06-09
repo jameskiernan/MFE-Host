d74fa410b21483f3e6a117c0348447fa91fc483b
JKiernan@cayan.com <JKiernan@cayan.com>
2021-06-09 13:47:57 -0400

    Added styling for demo

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { hostStyle, fragmentStyle } from './style';
 
 const Alert = React.lazy(() => import('app2/Alert'));
 
 const App = () => {
+
+  const [isVisible, setVisible] = useState(false);
+  const handleClick = () => {
+    setVisible(!isVisible);
+  }
+
   const alertContent = {
     title: 'Hello World',
-    text: 'This is text from App1 sent to App2 via props'
+    text: 'This is text from App1'
   }
   return (
-    <>
-      <h2 className="text-xl font-bold py-4">Hello from React component</h2>
-      <React.Suspense fallback='Loading Alert...'>
-        <Alert
-          alertTitle={alertContent.title}
-          alertText={alertContent.text}
-        />
-      </React.Suspense>
-    </>
+    <div style={hostStyle}>
+      <h2 className="text-5xl font-bold py-4">Hello from Host Container</h2>
+      <button
+        className="bg-blue-600 font-bold text-white px-4 py-3 rounded transition duration-300 ease-in-out hover:bg-blue-600 mr-6"
+        onClick={handleClick}>
+          Toggle Alert
+      </button>
+      <div className="fragment" style={fragmentStyle}>
+        { isVisible &&
+          <React.Suspense fallback='Loading Alert...'>
+            <Alert
+              alertTitle={alertContent.title}
+              alertText={alertContent.text}
+            />
+          </React.Suspense>
+        }
+      </div>
+    </div>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,17 @@
+export const hostStyle = {
+  border: '5px dashed #47B881',
+  width: '97vw',
+  height: '97vh',
+  padding: '50px',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  justifyContent: 'center'
+}
+export const fragmentStyle = {
+  border: '5px dashed #bfdcf3',
+  padding:'50px',
+  margin: '50px',
+  height: '250px',
+  minWidth: '700px'
+}
